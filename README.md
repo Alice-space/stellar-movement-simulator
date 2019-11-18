66c77224c8cabbc1dba183031e32fe186473914d
Alice-space <lizhihao0525@gmail.com>
2019-11-18 11:21:30 +0800

    star

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 <!--
  * @Author: Alicespace
  * @Date: 2019-11-08 14:55:19
- * @LastEditTime: 2019-11-11 09:36:40
+ * @LastEditTime: 2019-11-18 08:26:40
  -->
 
 # stellar-movement-simulator
 
 星体运动模拟器
 使用Panda3D引擎
+分析 开发 测试 维护
---
+++ main.py
@@ -0,0 +1,21 @@
+'''
+@Author: Alicespace
+@Date: 2019-11-18 08:06:30
+@LastEditTime: 2019-11-18 11:21:14
+'''
+from math import pi, sin, cos
+
+from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+from direct.actor.Actor import Actor
+from direct.interval.IntervalGlobal import Sequence
+from panda3d.core import Point3
+
+
+class stellarMovementSimulator(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+
+
+app = stellarMovementSimulator()
+app.run()
